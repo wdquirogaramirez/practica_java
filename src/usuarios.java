77d814bb06ec5cb4928cc2c9ef326309196ecd02
¨wdquirogaramirez¨ <gauchin@hotmail.com>
2021-11-09 18:32:42 -0300

    [add] Primer commit

---
+++ src/usuarios.java
@@ -0,0 +1,28 @@
+
+public class usuarios {
+    int usuarioID;
+    String nombre;
+    String apellido;
+    int edad;
+    String hobbie;
+    String editor_de_codigo;
+    String sistema_operativo;
+
+
+
+
+    public  usuarios() {
+        this.usuarioID = usuarioID;
+        this.nombre = nombre;
+        this.apellido =apellido;
+        this.edad = edad;
+        this.hobbie = hobbie;
+        this.editor_de_codigo = editor_de_codigo;
+        this.sistema_operativo = sistema_operativo;
+
+    }
+
+
+
+
+}
