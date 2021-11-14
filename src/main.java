6165282857cf2b462dd574d0a565890c775c398a
¨wdquirogaramirez¨ <gauchin@hotmail.com>
2021-11-14 18:47:50 -0300

    Mejorado los objetos

--- src/main.java
+++ src/main.java
@@ -1,39 +1,16 @@
-import java.util.Scanner;
 public class main {
+
     public static void main(String[] args) {
-           Scanner sc = new Scanner(System.in);
-            //nombre, apellido, edad, hobbie, editor de código preferido, sistema operativo que utiliza
-        usuarios nuevo;
-        nuevo = new usuarios();
+        usuarios nuevo = new usuarios();
 
-        System.out.println("Ingrese su nombre: ");
-        String dato = sc.next();
-        nuevo.nombre = dato;
-
-        System.out.println("Ingrese su Apellido: ");
-        dato = sc.next();
-        nuevo.apellido = dato;
-
-        System.out.println("Ingrese su edad: ");
-        int daton = sc.nextInt();
-        nuevo.edad = daton;
-
-        System.out.println("Ingrese su hobbie: ");
-        dato = sc.next();
-        nuevo.hobbie = dato;
-        
-        System.out.println("Ingrese su editor de código preferido: ");
-        dato = sc.next();
-        nuevo.editor_de_codigo = dato;
-
-        System.out.println("Ingrese su editor de sistema operativo que utiliza: ");
-        dato = sc.next();
-        nuevo.sistema_operativo = dato;
-
-
-        System.out.println("Gracias ! "+ nuevo.nombre + "  " +nuevo.apellido );
-        System.out.println("Tus preferencias son "+ nuevo.sistema_operativo +"  "+  nuevo.hobbie +"  " +nuevo.editor_de_codigo + " y tu edad es " +nuevo.edad);
-
+        nuevo.ConsultaNombre();
+        nuevo.ConsultaApellido();
+        nuevo.ConsultaEdad();
+        nuevo.ConsultaHobiee();
+        nuevo.ConsultaEditorDeCodigo();
+        nuevo.ConsultaSistemaOperativo();
+        nuevo.resumen();
         }
+
     }
 
--- src/usuarios.java
+++ src/usuarios.java
@@ -1,17 +1,16 @@
-
+import  java.util.Scanner;
 public class usuarios {
+    Scanner Sc = new Scanner(System.in);
+
     int usuarioID;
     String nombre;
     String apellido;
-    int edad;
+    String edad;
     String hobbie;
     String editor_de_codigo;
     String sistema_operativo;
 
-
-
-
-    public  usuarios() {
+    public  usuarios( int usuarioID, String nombre, String apellido, String edad, String hobbie, String editor_de_codigo, String sistema_operativo) {
         this.usuarioID = usuarioID;
         this.nombre = nombre;
         this.apellido =apellido;
@@ -22,7 +21,43 @@ public class usuarios {
 
     }
 
+  public usuarios() {
+
+  }
 
 
-
+    public void ConsultaNombre(){
+        System.out.println("Ingrese su nombre: ");
+        Sc.useDelimiter("\n");
+        this.nombre = Sc.next();
+    }
+    public  void ConsultaApellido(){
+        System.out.println("Tipee su Apellido: ");
+        Sc.useDelimiter("\n");
+        this.apellido = Sc.next();
+    }
+    public  void ConsultaEdad(){
+        System.out.println("Tipee su Edad: ");
+        Sc.useDelimiter("\n");
+        this.edad = Sc.next();
+    }
+    public  void ConsultaHobiee(){
+        System.out.println("Tipee su Hobiee: ");
+        Sc.useDelimiter("\n");
+        this.hobbie = Sc.next();
+    }
+    public  void ConsultaEditorDeCodigo(){
+        System.out.println("Tipee su Editor de codigo Favorito: ");
+        Sc.useDelimiter("\n");
+        this.editor_de_codigo = Sc.next();
+    }
+    public  void ConsultaSistemaOperativo(){
+        System.out.println("Tipee su Sistema Operativo: ");
+        Sc.useDelimiter("\n");
+        this.sistema_operativo = Sc.next();
+    }
+    public void  resumen (){
+        System.out.println("Gracias ! "+ this.nombre + "   " +this.apellido );
+        System.out.println("Tus preferencias son "+ this.sistema_operativo +",  "+  this.hobbie +",  " +this.editor_de_codigo + ", y tu edad es " +this.edad);
+    }
 }
