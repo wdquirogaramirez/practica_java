d5d90537b75072bc7e6b2bda03f9179392e3f0ba
¨wdquirogaramirez¨ <gauchin@hotmail.com>
2021-11-14 19:14:59 -0300

    [Add][Agrego la documentacion]

--- src/main.java
+++ src/main.java
@@ -10,7 +10,10 @@ public class main {
         nuevo.ConsultaEditorDeCodigo();
         nuevo.ConsultaSistemaOperativo();
         nuevo.resumen();
+
+
+
         }
 
-    }
+}
 
--- src/usuarios.java
+++ src/usuarios.java
@@ -1,8 +1,7 @@
 import  java.util.Scanner;
 public class usuarios {
     Scanner Sc = new Scanner(System.in);
-
-    int usuarioID;
+      int usuarioID;
     String nombre;
     String apellido;
     String edad;
@@ -24,38 +23,66 @@ public class usuarios {
   public usuarios() {
 
   }
-
-
+  /**
+   * consulta el nombre al usuario y lo guarda en el objeto usuario
+   *
+   * */
     public void ConsultaNombre(){
         System.out.println("Ingrese su nombre: ");
         Sc.useDelimiter("\n");
         this.nombre = Sc.next();
     }
+    /**
+     * consulta el Apellido al usuario y lo guarda en el objeto usuario
+     *
+     * */
     public  void ConsultaApellido(){
         System.out.println("Tipee su Apellido: ");
         Sc.useDelimiter("\n");
         this.apellido = Sc.next();
     }
+    /**
+     * consulta el Edad al usuario y lo guarda en el objeto usuario
+     *
+     * */
     public  void ConsultaEdad(){
         System.out.println("Tipee su Edad: ");
         Sc.useDelimiter("\n");
         this.edad = Sc.next();
     }
+    /**
+     * consulta el hobiee al usuario y lo guarda en el objeto usuario
+     *
+     * */
     public  void ConsultaHobiee(){
         System.out.println("Tipee su Hobiee: ");
         Sc.useDelimiter("\n");
         this.hobbie = Sc.next();
     }
+    /**
+     * consulta el editor de codigo  al usuario y lo guarda en el objeto usuario
+     *
+     * */
     public  void ConsultaEditorDeCodigo(){
         System.out.println("Tipee su Editor de codigo Favorito: ");
         Sc.useDelimiter("\n");
         this.editor_de_codigo = Sc.next();
     }
+    /**
+     * consulta el Sistema Operativo al usuario y lo guarda en el objeto usuario
+     *
+     * */
     public  void ConsultaSistemaOperativo(){
         System.out.println("Tipee su Sistema Operativo: ");
         Sc.useDelimiter("\n");
         this.sistema_operativo = Sc.next();
     }
+    /**
+     * Muestra el resumen
+     * de los datos  ingresados
+     * por en usuario
+     *
+     * */
     public void  resumen (){
         System.out.println("==>Gracias ! "+ this.nombre + "   " +this.apellido +"<==\n "+ "Tus preferencias son:  \n * "+ this.sistema_operativo +"\n * "+  this.hobbie +"\n * " +this.editor_de_codigo + "\n y tu edad es " +this.edad );
 
